fb97caa490964f44b559a13cae0618fb9db7fc42
Daniel Dörflinger <ddoerflinger@student.tugraz.at>
2022-07-21 12:22:45 +0200

    reworked audio tests: always check both sliders/sources and use fixture to test both main and pause audio menu

--- unity/Assets/Tests/PlayModeTests/AudioMenuTests.cs
+++ unity/Assets/Tests/PlayModeTests/AudioMenuTests.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
+using System.Linq;
 using GEAR.Localization;
 using NUnit.Framework;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using UnityEngine.UI;
 
@@ -14,134 +13,200 @@ using UnityEngine.UI;
 namespace Tests.PlayModeTests
 {
     using static PlaymodeTestUtils;
-    // TODO apply TestFixture with soundFx and music as inputs
     
-    [TestFixtureSource(nameof(_fixtureArgs))]
+    [TestFixture("MainMenu")]
+    [TestFixture("PauseMenu")]
     public class AudioMenuTests
     {
-        private static object [] _fixtureArgs = {
-            new object[] { "MusicSource", "preMenuButtonSliderMusic" },
-            new object[] { "SoundEffectSource", "preMenuButtonSliderFx" }
-        };
-        
-        private readonly string _audioSourceName;
-        private readonly string _sliderName;
-
-        private AudioSource _audioSourceComponent;
-        private Slider _audioSlider;
+        private readonly string _menuType;
+        private bool _sceneLoaded;
         private Button _audioButton;
         
-        private const string MainMenuScenePath = "Assets/Maroon/scenes/special/MainMenu.pc.unity";
+        private const string MusicAudioSourceName = "MusicSource";
+        private const string FxAudioSourceName = "SoundEffectSource";
+        private const string MusicSliderName = "preMenuButtonSliderMusic";
+        private const string FxSliderName = "preMenuButtonSliderFx";
 
-        // A constructor is required for TestFixtureSource to pass arguments
-        public AudioMenuTests(string audioSourceName, string sliderName)
+        private AudioSource _musicAudioSourceComponent;
+        private AudioSource _fxAudioSourceComponent;
+        private Slider _musicAudioSliderComponent;
+        private Slider _fxAudioSliderComponent;
+
+        public AudioMenuTests(string menuType)
         {
-            _audioSourceName = audioSourceName;
-            _sliderName = sliderName;
+            _menuType = menuType;
+            _sceneLoaded = false;
         }
-
+        
+        
+        
         [UnitySetUp]
         public IEnumerator SetUp()
         {
-            // TODO extract load main menu to util method and use everywhere
-            
-            // Start from Main Menu for every following test
-            yield return EditorSceneManager.LoadSceneAsyncInPlayMode(MainMenuScenePath, new LoadSceneParameters(LoadSceneMode.Single));
+            // Workaround for missing UnityOneTimeSetUp to load scene only once for faster test execution
+            // This code is run only once for the whole test fixture
+            // https://forum.unity.com/threads/add-coroutine-version-of-onetimesetup.890092/
+            if (!_sceneLoaded)
+            {
+                // Load scene
+                if (_menuType == "MainMenu")
+                {
+                    yield return LoadMainMenuScene();
+                }
+                else if (_menuType == "PauseMenu")
+                {
+                    yield return LoadFallingCoilScene();
+                }
+                
+                _sceneLoaded = true;
+                
+                // Enter playmode to initialize scene
+                yield return new EnterPlayMode();
 
-            var currentSceneName = SceneManager.GetActiveScene().name;
-            Assert.AreEqual("MainMenu.pc", currentSceneName, "'MainMenu.pc' scene did not load");
+                // Pause menu requires activating the menu (usually done by pressing ESC)
+                // Look into Input Tests and try simulating ESC keypress if possible
+                if (_menuType == "PauseMenu")
+                {
+                    var canvasGameObject = FindInActiveGameObjectByName("Canvas");
+                    Assert.NotNull(canvasGameObject);
+                    Assert.AreEqual(canvasGameObject.transform.parent.name, "PauseMenu");
+                    canvasGameObject.SetActive(true);
+                    yield return null;
+                }
+                
+                // Find audio submenu button
+                string mainMenuAudioButtonLabel = LanguageManager.Instance.GetString("Menu Audio");
+                _audioButton = GetButtonViaText(mainMenuAudioButtonLabel);
+            }
             
-            // Enter playmode to initialize scene (especially the button labels)
-            yield return new EnterPlayMode();
-            
-            // Click Audio button to open audio menu
-            string mainMenuAudioButtonLabel = LanguageManager.Instance.GetString("Menu Audio");
-            _audioButton = GetButtonViaText(mainMenuAudioButtonLabel);
+            // The code below is run before every test case in the test fixture
+            // Open/reset audio menu
             _audioButton.onClick.Invoke();
             
-            yield return null;
+            // Get AudioSource components
+            _musicAudioSourceComponent = GetComponentFromGameObjectWithName<AudioSource>(MusicAudioSourceName);
+            _fxAudioSourceComponent = GetComponentFromGameObjectWithName<AudioSource>(FxAudioSourceName);
             
-            var audioSourceGameObject = GameObject.Find(_audioSourceName);
-            Assert.NotNull(audioSourceGameObject, $"Could not find GameObject '{_audioSourceName}'");
-
-            _audioSourceComponent = audioSourceGameObject.GetComponent<AudioSource>();
-            Assert.NotNull(_audioSourceComponent, $"Could not find AudioSource component in GameObject '{_audioSourceName}'");
-            
-            _audioSlider = GetComponentInChildrenFromGameObjectWithName<Slider>(_sliderName);
+            // Get Slider components
+            _musicAudioSliderComponent = GetComponentInChildrenFromGameObjectWithName<Slider>(MusicSliderName);
+            _fxAudioSliderComponent = GetComponentInChildrenFromGameObjectWithName<Slider>(FxSliderName);
         }
         
-        [UnityTearDown]
-        public IEnumerator TearDown()
+        [UnityTest, Order(1), Description("On opening audio menu, slider values and audio sources' volume must match")]
+        public IEnumerator WhenOpenAudioMenuInitialSliderValuesEqualAudioSourceVolume()
         {
-            // TODO not necessary?
-            yield return new ExitPlayMode();
-        }
-        
-        [UnityTest, Order(1), Description("On opening audio menu, slider value and audio source volume must match")]
-        public IEnumerator WhenOpenAudioMenuInitialSliderValueEqualsAudioSourceVolume()
-        {
-            var audioSourceInitialVolume = _audioSourceComponent.volume;
-            var audioSliderInitialValue = _audioSlider.value;
-
-            Assert.AreEqual(audioSourceInitialVolume, audioSliderInitialValue, 0.0, 
-                $"Initial values should be equal: slider '{_sliderName}' value and audio source '{_audioSourceName}' volume");
+            // Note:
+            // Would like to use https://docs.nunit.org/articles/nunit/writing-tests/assertions/multiple-asserts.html
+            // But Assert.Multiple() is not (yet) supported by Unity test framework :(
+            // Source: https://stackoverflow.com/a/52231966/7262963
+            Assert.AreEqual(_musicAudioSliderComponent.value, _musicAudioSourceComponent.volume, 0.0, 
+                $"Initial Slider and AudioSource values must be equal");
             
+            Assert.AreEqual(_fxAudioSliderComponent.value, _fxAudioSourceComponent.volume, 0.0, 
+                $"Initial Slider and AudioSource values must be equal");
             yield return null;
         }
         
-        [UnityTest, Order(2), Description("Change the audio slider's value, then the audio source's volume must match")]
-        public IEnumerator WhenChangeSliderValueThenAudioSourceVolumeMatches()
+        [UnityTest, Order(2), Description("Change the music audio slider's value, then the music audio source's volume must match")]
+        public IEnumerator WhenChangeMusicSliderValue_ThenMusicAudioSourceVolumeMatches()
         {
-            _audioSlider.value = 0.5f;
+            _musicAudioSliderComponent.value = 0.6f;
             
             yield return null;
             
-            var audioSourceVolume = _audioSourceComponent.volume;
-            
-            Assert.AreEqual(_audioSlider.value, audioSourceVolume, 0.0, 
-                $"After slider '{_sliderName}' value change, mismatched audio source '{_audioSourceName}' volume");
+            Assert.AreEqual(_musicAudioSliderComponent.value, _musicAudioSourceComponent.volume, 0.0, 
+                $"After {MusicSliderName} value change, unexpected audio source '{MusicAudioSourceName}' volume");
         }
         
-        [UnityTest, Order(3), Description("Change the audio slider's value multiple times, then the audio source's volume must match")]
-        public IEnumerator WhenChangeSliderValueMultipleTimesThenAudioSourceVolumeMatches()
+        [UnityTest, Order(3), Description("Change the FX audio slider's value, then the FX audio source's volume must match")]
+        public IEnumerator WhenChangeFxSliderValueThenFxAudioSourceVolumeMatches()
         {
-            float[] volumeLevels = { 0f,  0.25f, 1/3f, 2/3f, 0.75f, 1f };
-
-            // https://docs.nunit.org/articles/nunit/writing-tests/assertions/multiple-asserts.html
-            // Assert multiple not supported by Unity test framework :(
-            foreach (var volumeLevel in volumeLevels)
+            _fxAudioSliderComponent.value = 0.5f;
+            
+            yield return null;
+            
+            Assert.AreEqual(_fxAudioSliderComponent.value, _fxAudioSourceComponent.volume, 0.0, 
+                $"After {FxSliderName} value change, unexpected audio source '{FxAudioSourceName}' volume");
+        }
+        
+        [UnityTest, Order(4), Description("Change both audio slider's value independently, then both audio sources' volume must match their slider")]
+        public IEnumerator WhenChangeBothSliderValuesIndependently_ThenBothAudioSourcesVolumeLevelsMatch()
+        {
+            _fxAudioSliderComponent.value = 0.1f;
+            _musicAudioSliderComponent.value = 0.2f;
+            
+            Assert.AreEqual(_musicAudioSliderComponent.value, _musicAudioSourceComponent.volume, 0.0, 
+                $"After changing both sliders, '{MusicSliderName}' value should match '{MusicAudioSourceName}' volume");
+            
+            Assert.AreEqual(_fxAudioSliderComponent.value, _fxAudioSourceComponent.volume, 0.0, 
+                $"After changing both sliders, '{FxSliderName}' value should match '{FxAudioSourceName}' volume");
+            yield return null;
+        }
+        
+        
+        [UnityTest, Order(5), Description("Change both audio sliders' values multiple times independently, then their audio sources' volumes must match")]
+        public IEnumerator WhenChangeSliderValuesIndependentlyMultipleTimes_ThenBothAudioSourcesVolumeLevelsMatch()
+        {
+            float[] expectedMusicVolumeLevels = { 0f,  0.25f, 1/3f, 0.5f, 2/3f, 0.75f, 1f };
+            float[] expectedFxVolumeLevels = expectedMusicVolumeLevels.Reverse().ToArray();
+            Assert.AreEqual(expectedMusicVolumeLevels.Length, expectedFxVolumeLevels.Length);
+            
+            for (int i = 0; i < expectedMusicVolumeLevels.Length; i++)
             {
-                _audioSlider.value = volumeLevel;
+                var expectedMusicVolume = expectedMusicVolumeLevels[i];
+                var expectedFxVolume = expectedFxVolumeLevels[i];
+                
+                // Set slider value
+                _musicAudioSliderComponent.value = expectedMusicVolume;
+                _fxAudioSliderComponent.value = expectedFxVolume;
         
                 yield return null;
-        
-                var audioSourceVolume = _audioSourceComponent.volume;
                 
-                Assert.AreEqual(_audioSlider.value, audioSourceVolume, 0.0, 
-                    $"After slider '{_sliderName}' value change, mismatched audio source '{_audioSourceName}' volume");
+                Assert.AreEqual(expectedMusicVolume, _musicAudioSliderComponent.value, 0.0, 
+                    $"After audio volume change, unexpected slider '{MusicSliderName}' value");
+                
+                Assert.AreEqual(expectedMusicVolume, _musicAudioSourceComponent.volume, 0.0, 
+                    $"After audio volume change, unexpected audio source '{MusicAudioSourceName}' volume");
+                
+                Assert.AreEqual(expectedFxVolume, _fxAudioSliderComponent.value, 0.0, 
+                    $"After audio volume change, unexpected slider '{FxSliderName}' value");
+                
+                Assert.AreEqual(expectedFxVolume, _fxAudioSourceComponent.volume, 0.0, 
+                    $"After audio volume change, unexpected audio source '{FxAudioSourceName}' volume");
             }
         }
         
-        [UnityTest, Order(4), Description("Change the audio slider's value, then reload menu, the audio source's volume must match")]
-        public IEnumerator WhenChangeSliderValueThenReloadMenuAudioSourceVolumeMatches()
+        [UnityTest, Order(6), Description("Change audio sliders' values, then reload menu, the audio sources' volume must match")]
+        public IEnumerator WhenChangeSlidersValuesAndReloadSubMenu_ThenAudioSourcesVolumeLevelsMatch()
         {
-            var expectedVolume = 0.5f;
-            _audioSlider.value = expectedVolume;
+            var expectedMusicVolume = 0.8f;
+            var expectedFxVolume = 0.7f;
+
+            _musicAudioSliderComponent.value = expectedMusicVolume;
+            _fxAudioSliderComponent.value = expectedFxVolume;
             
             yield return null;
             
-            // Reload menu
+            // Reload the menu: destroys the audio menu and its child objects (slider)
             _audioButton.onClick.Invoke();
             
             yield return null;
             
-            // Get newly created slider
-            _audioSlider = GetComponentInChildrenFromGameObjectWithName<Slider>(_sliderName);
+            // Get newly created sliders
+            _musicAudioSliderComponent = GetComponentInChildrenFromGameObjectWithName<Slider>(MusicSliderName);
+            _fxAudioSliderComponent = GetComponentInChildrenFromGameObjectWithName<Slider>(FxSliderName);
 
-            Assert.AreEqual(expectedVolume, _audioSlider.value, 0.0, 
-                $"After audio menu reload, unexpected slider '{_sliderName}' value");
-            Assert.AreEqual(expectedVolume, _audioSourceComponent.volume, 0.0, 
-                $"After audio menu reload, unexpected audio source '{_audioSourceName}' volume");
+            Assert.AreEqual(expectedMusicVolume, _musicAudioSliderComponent.value, 0.0, 
+                $"After audio menu reload, unexpected slider '{MusicSliderName}' value");
+            
+            Assert.AreEqual(expectedMusicVolume, _musicAudioSourceComponent.volume, 0.0, 
+                $"After audio menu reload, unexpected audio source '{MusicAudioSourceName}' volume");
+            
+            Assert.AreEqual(expectedFxVolume, _fxAudioSliderComponent.value, 0.0, 
+                $"After audio menu reload, unexpected slider '{FxSliderName}' value");
+            
+            Assert.AreEqual(expectedFxVolume, _fxAudioSourceComponent.volume, 0.0, 
+                $"After audio menu reload, unexpected audio source '{FxAudioSourceName}' volume");
         }
     }
 }
